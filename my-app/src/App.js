4d9fa76d04f0db14dd952c5bf740ac4f4d1c8429
Divyanshu-kum <hlodivyanshu@gmail.com>
2025-09-19 07:28:15 +0530

    my-app code

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from './chai';
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1> chai aur code</h1>
+    <h2> My name is Divyanshu Kumar </h2>
+    </>
+  );
+}
+
+export default App;

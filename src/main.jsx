f7d342ab9025d5ec0d2e659ce0895c21b58dcff5
Divyanshu-kum <hlodivyanshu@gmail.com>
2025-09-22 11:55:31 +0530

    01vitereact

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,32 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+function MyApp(){
+  return (
+    <div>
+      <h1> Custom App!</h1>
+    </div>
+  )
+}
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: 'https://google.com',
+        target: '_blank'
+    },
+    children: 'Click me to visit google'
+}
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+    </a>
+)
+
+createRoot(document.getElementById('root')).
+render(
   <StrictMode>
-    <App />
+    <MyApp />
   </StrictMode>
 );
